f53d61b597ac553fab0ba955da46dbd0e9c9922c
Tsege <tsegeer@ad.vzwcorp.com>
2021-12-22 17:03:36 -0600

    ET:Login and singup

--- initialiseDb.js
+++ initialiseDb.js
@@ -3,13 +3,17 @@ const db = require('./db')
 const Pallet  = require('./models/pallet')
 const  Warehouse  = require('./models/warehouse')
 const  Box  = require('./models/box')
+const Employee = require('./models/Employee')
 
 //associate models
 async function initialiseDb() {
 Warehouse.hasMany(Pallet)
+
 Pallet.belongsTo(Warehouse)
 Pallet.hasMany(Box)
 Box.belongsTo(Pallet)
+Warehouse.hasMany(Employee)
+Employee.belongsTo(Warehouse)
 await db.sync();
 }
 //export models with added associations
